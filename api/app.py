07275cea7c4ee541b2b7103d492de9a205fc66e2
Nine Kinnman <nk1520@ic.ac.uk>
2023-10-19 14:31:51 +0100

    fixing  age calcutator app

--- api/app.py
+++ api/app.py
@@ -4,17 +4,17 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
 
 @app.route("/calculate_age", methods=["POST"])
 def calculate_age():
-    input_name = request.form.get("name")
-    input_dob = request.form.get("dob")
+    name = request.form.get("name")
+    dob = request.form.get("dob")
 
     dob_date = datetime.strptime(dob, "%Y-%m-%d")
 
     today = datetime.now()
     age_in_days = (today - dob_date).days
 
-    return render_template("hello.html", name=input_name, dob=input_dob)
+    return f"Hello, {name}! You are {age_in_days} days old."
