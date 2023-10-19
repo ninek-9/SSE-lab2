73b4a990735c418a0f41d9d148e8ac5e4585e895
Nine Kinnman <nk1520@ic.ac.uk>
2023-10-19 14:18:07 +0100

    creating age calcutator app

--- api/app.py
+++ api/app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
-@app.route("/submit", methods=["POST"])
-def submit():
+@app.route("/calculate_age", methods=["POST"])
+def calculate_age():
     input_name = request.form.get("name")
-    input_age = request.form.get("age")
-    return render_template("hello.html", name=input_name, age=input_age)
+    input_dob = request.form.get("dob")
+
+    dob_date = datetime.strptime(dob, "%Y-%m-%d")
+
+    today = datetime.now()
+    age_in_days = (today - dob_date).days
+
+    return render_template("hello.html", name=input_name, dob=input_dob)
