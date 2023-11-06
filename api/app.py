5b657bbc903289ae1c10c1135b8e946da5dfb03a
Nine Kinnman <nk1520@ic.ac.uk>
2023-11-06 16:20:50 +0000

    fixing error app.py

--- api/app.py
+++ api/app.py
@@ -22,7 +22,7 @@ def calculate_age():
     return render_template("hello.html", name=name, age_in_days=age_in_days)
 
 
-@app.route("/github_username", methods=["POST"])
+@app.route("/submit", methods=["POST"])
 def github_username():
     username = request.form.get("username")
 
