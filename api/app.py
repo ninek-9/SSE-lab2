fbad9cec0ca31e21e05efae39175bf555673fc0e
pi320 <pi320@ic.ac.uk>
2023-11-07 10:58:19 +0000

    contributors url fix indentations 5

--- api/app.py
+++ api/app.py
@@ -64,7 +64,7 @@ def username():
                 contributors = contributors_response.json()
                 repo["contributors"] = [{
                     "name": contributor["login"],
-                    "commits": contributor["contributions"]} 
+                    "commits": contributor["contributions"]}
                     for contributor in contributors[:5]]
 
             else:
